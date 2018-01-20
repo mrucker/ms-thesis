0233d3cb5751fb87a39db1ea4550297607104ec7
Mark Rucker <rucker.mark@gmail.com>
2018-01-19 21:23:21 -0500

    It Is WORKING!!!!!!!

--- pipeline/experiment/experiment.js
+++ pipeline/experiment/experiment.js
@@ -1,11 +1,24 @@
 function Experiment(participant)
 {
-    var id = 0;
+    var id = generateId();        
+    var that = this;
+        
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "https://api.thesis.markrucker.net/v1/participants/" + participant.getId() + "/experiments", true);
+    xhttp.send(id);
     
     this.beginExperiment = function() {
-        //create an experiment in dynamoDB passing along participant.getId();
-        //set the id value to the dynamoDB.Id;
-        //begin passive background observing
+        
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("PATCH", "https://api.thesis.markrucker.net/v1/participants/" + participant.getId() + "/experiments/" + id, true);
+        xhttp.send('{"startTime":"' + new Date().toUTCString() +'"}');
+    }    
+    
+    this.endExperiment = function () {
+        
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("PATCH", "https://api.thesis.markrucker.net/v1/participants/" + participant.getId() + "/experiments/" + id, true);
+        xhttp.send('{"stopTime":"' + new Date().toUTCString() +'"}');
     }
     
     function makeObservation() {
@@ -16,8 +29,13 @@ function Experiment(participant)
         
     };
     
-    this.endExperiment = function () {
-        //get the completion code from dynamodb
-        //use lambda functions to double check runtime.
+    //17 bytes in storage size
+    function generateId() {
+        //r1 = Final value represents a number between 0 and 4.295 billion (we remove characters and convert to hex to save space)
+        //r2 = Final value represents a number between 0 and 795.36 days worth of miliseconds (we remove characters and convert to hex to save space)
+        var r1 = Math.floor(Math.random()*Math.pow(10,16)).toString(16).substring(0,8); 
+        var r2 = Date.now().toString(16).substring(2);
+
+        return r1 + r2;
     }
 }
--- pipeline/experiment/participant.js
+++ pipeline/experiment/participant.js
@@ -1,10 +1,16 @@
 function Participant(canvas)
-{
-    //17 bytes per user id in dynamodb (dynamodb uses UTF-8 which encodes alll ASCII characters in 8 bits)    
-    var id = readCookie() || generateId();
+{    
+    var id = readCookie();
+    
+    if(!id) {
+        id = generateId();
+        
+        var xhttp = new XMLHttpRequest();        
+        xhttp.open("POST", "https://api.thesis.markrucker.net/v1/participants", true);
+        xhttp.send(id);
+    }
     
     writeCookie(id);
-    writeDynamo(id);
     
     this.getId = function() { return id };
     
@@ -16,16 +22,15 @@ function Participant(canvas)
         document.cookie = val;
     }
     
-    function writeDynamo(id) {
-        //here is where I'd write my the participantId to dynamodb
-    }
-    
+    //17 bytes in storage size
     function generateId() {
         //r1 = Final value represents a number between 0 and 4.295 billion (we remove characters and convert to hex to save space)
         //r2 = Final value represents a number between 0 and 795.36 days worth of miliseconds (we remove characters and convert to hex to save space)
         var r1 = Math.floor(Math.random()*Math.pow(10,16)).toString(16).substring(0,8); 
         var r2 = Date.now().toString(16).substring(2);
-    
+
         return r1 + r2;
     }
+    
+    
 }
