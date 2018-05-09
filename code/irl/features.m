c6488d39c6b711a510144b31f688f1183b5588f1
Mark Rucker <rucker.mark@gmail.com>
2018-05-09 17:38:55 -0400

    Fixed some bugs with my pruning algorithm. Improved the feature calculation structure. Removed the kernel approximation of the feature transform because it didn't seem to help.

--- code/irl/features.m
+++ code/irl/features.m
@@ -1,32 +1,31 @@
-function f = features(cursorData, targetData)
+function f = features(locationData, historyData, targetData)
 
     %cursorData = [c1_x,c1_y;...;c4_x,c4_y]
     %targetData = [t1_x,t1_y,t1_age;...;tn_x,tn_y,tn_age]
     
-    assert(mod(numel(cursorData),2) == 0, 'incorrect amount of cursor history passed in for feature calculation');
-    assert(mod(numel(targetData),3) == 0, 'incorrect target data, expected: (x,y,age) for each target');    
+    assert(mod(numel(locationData) ,2) == 0, 'at least one cursor location has only a single x or y');
+    assert(mod(numel(historyData)  ,6) == 0, 'incorrect amount of cursor history passed in for feature calculation');
+    assert(mod(numel(targetData)   ,3) == 0, 'incorrect target data, expected: (x,y,age) for each target');    
     
-    numLengths = numel(cursorData)/2;
-    numTargets = numel(targetData)/3;
+    locationCount = numel(locationData)/2;
+    targetCount   = numel(targetData)/3;
     
-    cursorData = reshape(cursorData, [], numLengths);
-    targetData = reshape(targetData, [], numTargets);
+    locationData = reshape(locationData, [], locationCount);
+    historyData  = reshape(historyData , [], 3);
+    targetData   = reshape(targetData  , [], targetCount);
 
     targetRadius = 150;%this value comes from targets.js
-    targetCount  = size(targetData,2);
-    
-    %[d1,d2,d3,d4,touched,age]
-    f = zeros(numLengths+2,1);
-    
-    x1 = cursorData;
+
+    x1 = [locationData,historyData];
     x2 = targetData(1:2,:);
     
-    distances = sqrt(dot(x2,x2,1)+dot(x1,x1,1)'-2*(x1'*x2));
-
-    f(1:numLengths) = sum(distances,2);
-    f(numLengths+1) = sum(distances(1,:) < targetRadius);
-    f(numLengths+2) = sum(targetData(3,:));       
+    targetDistances   = sqrt(dot(x2,x2,1)+dot(x1,x1,1)'-2*(x1'*x2));    
+    averageDistances  = sum(targetDistances,2)/targetCount;    
+    locationDistances = averageDistances(1:end-3)';
+    historyDistances  = averageDistances(end-2:end);
+    targetTouches     = sum(targetDistances(1:end-3,:) < targetRadius,2)'/targetCount;
+    targetAges        = sum(targetData(3,:))/targetCount;
     
-    f = f/targetCount;
-    
+    %[d1,d2,d3,d4,age,touched]
+    f = vertcat(locationDistances, repmat(vertcat(historyDistances,targetAges),1,locationCount), targetTouches); 
 end
--- code/irl/main.m
+++ code/irl/main.m
@@ -1,5 +1,7 @@
 add_ourpaths;
 
-trajectory = jsondecode(fileread('trajectory.json'));
-params     = struct ('kernel',1, 'epsilon',.001, 'gamma',.9, 'sigma',1, 'steps',4, 'seed',1);
+trajectory = jsondecode(fileread('trajectory-test.json'));
+params     = struct ('kernel',1, 'epsilon',.001, 'gamma',.9, 'sigma',1, 'steps',4, 'seed',1, 'maxdist',15, 'maxage',1000);
 result     = algorithm5run(params, trajectory, 1);
+
+%norm([3175,1535]) == 3526.6
