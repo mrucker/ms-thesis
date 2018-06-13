314d7b4003899f32045fa8a7e9fa32d05fb8b2fa
Mark Rucker <rucker.mark@gmail.com>
2018-06-12 20:22:58 -0400

    sorting and flattening observations from an experiment

--- code/amazon/dynamodb/DocumentClient Queries/ThesisObservations.js
+++ code/amazon/dynamodb/DocumentClient Queries/ThesisObservations.js
@@ -14,6 +14,41 @@ var params = {
 var documentClient = new AWS.DynamoDB.DocumentClient();
 
 documentClient.query(params, function(err, data) {
-   if (err) console.log(err);
-   else     console.log(data.Items.map(d => JSON.stringify(d.Observations)));
+   if (err) {
+       console.log(err);
+   } 
+   else {
+        var sortedObservations    = data.Items.sort((i1,i2) => i1.SequenceNumber - i2.SequenceNumber).map(i => i.Observations);
+        var flattenedObservations = [].concat.apply([], sortedObservations);
+
+        console.log(JSON.stringify(flattenedObservations));
+   }
 });
+
+ function toStates(observations) {
+    var states = observations.slice(3).map(function(observation, i) {
+
+        var mp_0 = observations[3+i-0].slice(0,2); //gives us [x,y];
+        var mp_1 = observations[3+i-1].slice(0,2); //gives us [x,y];
+        var mp_2 = observations[3+i-2].slice(0,2); //gives us [x,y];
+        var mp_3 = observations[3+i-3].slice(0,2); //gives us [x,y];
+
+        return mp_0.concat(mp_1).concat(mp_2).concat(mp_3).concat(observation.slice(2));
+    });
+    
+    return states;
+}
+
+function toActions(observations) {
+    var actions = observations.slice(3).map(function(obs, i) {
+
+        var thisState_x = observations[3+i-0][0];
+        var thisState_y = observations[3+i-0][1];
+        var prevState_x = observations[3+i-1][0];
+        var prevState_y = observations[3+i-1][1];
+
+        return i == 0 ? [0,0] : [ thisState_x - prevState_x, thisState_y - prevState_y ];
+    });
+    
+    return actions;
+}
