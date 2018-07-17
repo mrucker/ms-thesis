6fe2da2a4baa37092adf206d2bff4695b01c27db
Mark Rucker <rucker.mark@gmail.com>
2018-07-17 18:49:46 -0400

    working more or less

--- code/algorithm/_utilities/exact/one_step.m
+++ code/algorithm/_utilities/exact/one_step.m
@@ -1,20 +1,14 @@
-function P = one_step(states, state2index, actions, time_on_screen, expected_interarrival)
+function P = one_step(states, state2index, actions, ticks, time_on_screen, expected_interarrival)
     P = cell(1,size(actions,2));
 
     for a_i = 1:size(actions,2)
         P{a_i} = sparse(size(states,2), size(states,2));
     end
-    
-    ticks = 33;
+
     max_targs = size(states(:,1),1) - 11;
 
-    tst_pmf = my_pmf(ticks, max_targs, time_on_screen, expected_interarrival);
-    t = my_pmf(1, 4, 2, 2);
-    sub_pmf = my_sub_pmf(ticks, max_targs, time_on_screen);
-    add_pmf = my_add_pmf(ticks, expected_interarrival);
-    %sub_add = vertcat(reshape(repmat([0 1 2 3], [4, 1]), [1, 16]), repmat([0 1 2 3], [1 4]));
-    
-    %I need a way to turn a state into its index
+    add_sub_sty_pmf = my_add_sub_sty_pmf(ticks, max_targs, time_on_screen, expected_interarrival);
+
     for a_i = 1:size(actions,2)
         for s_i = 1:size(states,2)
 
@@ -25,9 +19,80 @@ function P = one_step(states, state2index, actions, time_on_screen, expected_int
             s(3:8) = s(1:6);
             s(1:2) = a;
 
-            P{a_i}(s_i,:) = my_sub_add(P{a_i}(s_i,:), s, sub_pmf, add_pmf, state2index);
+            P{a_i}(s_i,:) = my_sub_add(P{a_i}(s_i,:), s, add_sub_sty_pmf, state2index);
         end
     end
+end
+
+function P = my_sub_add(P, s, add_sub_sty_pmf, state2index)
+
+    x = s(12:end);
+    a = dec2bin((1:2^numel(x))-1)' - '0';
+
+    add_k = sum(a-x == +1);
+    sub_k = sum(a-x == -1);
+    sty_k = sum(a+x == +2);
+
+    states   = vertcat(repmat(s(1:11), [1 size(a,2)]), a);
+    states_i = state2index(states);
+
+    states_p = arrayfun(@(s_i) factorial(add_k(s_i)) * add_sub_sty_pmf(add_k(s_i), sub_k(s_i), sty_k(s_i)), 1:size(a,2));
+
+    if all(x == 1)
+        add_k = add_k + 1;
+        states_p = states_p + arrayfun(@(s_i) factorial(add_k(s_i)) * add_sub_sty_pmf(add_k(s_i), sub_k(s_i), sty_k(s_i)), 1:size(a,2));        
+    end
+    
+    states_i = states_i(states_p > 0);
+    states_p = states_p(states_p > 0); %I just hope this keeps them in order
+    
+    P(states_i) = P(states_i) + states_p;
+end
+
+function pmf = my_add_sub_sty_pmf(ticks, max_targs, time_on_screen, expected_interarrival)
+    
+    %adds  \in [0, max_adds]
+    %subs  \in [0, max_subs]
+    
+    %puts  \in [0, max_adds]
+    %stays \in [0, max_subs]
+    
+    add_n = ticks;
+    add_k = 0:add_n;
+
+    add_prob = 1/expected_interarrival;
+
+    sub_prob = 1/time_on_screen;
+    sty_prob = 1 - sub_prob;
+
+    %This is the probability of k being added in n ticks at k specific locations.
+
+    add_k_prob = my_nchoosek(add_n, add_k) .* add_prob.^add_k .* (1-add_prob).^(add_n-add_k);
+    add_k_prob = [add_k_prob, zeros(1,50)]; %we add a bunch of zeros in case somebody tries to add more than is possible   
+    
+    %intentionally changed my put distribution so that it no longer
+    %considers with replacement. This makes calculations much faster and
+    %simpler since I don't have to enumerate combinations.
+    %for historical purposes the old pmf was (1/max_targs).^(add_k);
+    put_k_prob = tril(ones(ticks+10, max_targs+1));
+    for adds = 0:(ticks+9)
+        for open  = 0:max_targs
+            if(open >= adds)
+                put_k_prob(adds+1,open+1) = factorial(open-adds)/factorial(open);
+            end
+        end
+    end
+    
+    %Two outcomes can happen to a target in a step, it either goes away or
+    %stays. Therefore these add up to one. However, because targets operate
+    %independently at each tick and once they are gone they stay gone, we
+    %don't need nchoosek. We just need the partial geometric series for all
+    %sequences that end in a subtraction event. We can then raise this to a
+    %power since each target's probability is independent of all others.
+    sub_ticks_prob = sub_prob * (1-sty_prob^ticks)/(1-sty_prob);
+    sty_ticks_prob = sty_prob^ticks;
+
+    pmf = @(add_k,sub_k,sty_k) add_k_prob(add_k+1) * put_k_prob(add_k + 1, max_targs - (sub_k + sty_k) + 1) * sub_ticks_prob^(sub_k) * sty_ticks_prob^(sty_k);
 end
 
 function v = my_nchoosek(n,k)
@@ -62,66 +127,4 @@ function sub_pmf = my_sub_pmf(ticks, max_targs, time_on_screen)
         sub_probability    = take_prob.^sub .* keep_prob.^(targs-sub);
         sub_pmf(targs+1,:) = [sub_combinations .* sub_probability, zeros(1,max_targs-targs)];
     end
-end
-
-function P = my_sub_add(P, s, sub_pmf, add_pmf, state2index)
-
-    sub_pmf = sub_pmf(sum(s(12:end)) + 1, :);
-
-    x = s(12:end);
-    a = dec2bin((1:2^numel(x))-1)' - '0';
-
-    sub_counts     = sum(a-x == -1);
-    add_counts     = sum(a-x == +1);
-    sub_add_counts = sum(a+x == +2);
-    
-    %sub_prob = 1 - (999/1000)^33;
-    %add_prob = 
-    
-    sub_spot_count = sum(x == 1);
-    add_spot_count = sum(x == 0);
-
-    sub_normalizer = 1./[1, my_nchoosek(sub_spot_count,1:sub_spot_count)];
-    add_normalizer = 1./[1, my_nchoosek(add_spot_count,1:add_spot_count)];
-
-    states   = vertcat(repmat(s(1:11), [1 2^numel(x)]), a);
-    states_i = state2index(states);
-        
-    %keep_prob = (1 - 1/1000)^33;
-    %take_prob = 1/1000 * ((1-(1-1/1000)^33)/(1-(1-1/1000)));
-    
-    states_p = floor(sub_pmf(sub_counts+1) .* sub_normalizer(sub_counts+1) .* add_pmf(add_counts+1) .* add_normalizer(add_counts+1) * 10000) / 10000;
-    %states_p = floor( add_pmf(add_counts+1) .* (1/6) .^ add_counts .* * 10000) / 10000
-
-    states_i = states_i(states_p > 0);
-    states_p = states_p(states_p > 0); %I just hope this keeps them in order
-    
-    P(states_i) = P(states_i) + states_p;
-end
-
-function pmf = my_pmf(ticks, max_targs, time_on_screen, expected_interarrival)
-    
-    %adds  \in [0, max_adds]
-    %subs  \in [0, max_subs]
-    
-    %puts  \in [0, max_adds]
-    %stays \in [0, max_subs]
-
-    max_adds = ticks;
-    max_subs = max_targs;
-    
-    add_cnt = 0:max_adds;
-    sub_cnt = 0:max_subs;
-    
-    add_prob = 1/expected_interarrival;
-    sub_prob = 1/time_on_screen;
-    sty_prob = 1 - sub_prob;
-    put_prob = 1/max_targs;
-
-    add_pmf = my_nchoosek(max_adds, add_cnt) .* add_prob.^add_cnt .* (1-add_prob).^(max_adds-add_cnt);
-    sub_pmf = ((1-sty_prob^ticks)/(1-sty_prob) * sub_prob).^(sub_cnt);
-    put_pmf = put_prob.^(add_cnt); %this could be improved to take into consideration add > put (that is we place on top of ourselves)
-    sty_pmf = sty_prob.^(ticks*sub_cnt);
-
-    pmf = @(adds,subs,stays) add_pmf(adds+1) * put_pmf(adds+1) * sub_pmf(subs+1) * sty_pmf(stays+1);
 end
