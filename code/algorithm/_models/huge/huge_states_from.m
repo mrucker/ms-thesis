35ccaaca93ea55567289ab68fa6d47851d9ebbf5
Mark Rucker <rucker.mark@gmail.com>
2018-07-21 11:57:36 -0400

    OCD

--- code/algorithm/_models/huge/huge_states_from.m
+++ code/algorithm/_models/huge/huge_states_from.m
@@ -1,4 +1,4 @@
-function s = huge_states_from(observations)    
+function s = huge_states_from(observations)
 
     %assumed observation = [x, y, w, h, r, \forall targets {x, y, age}]
     assert(all(cellfun(@(o) isnumeric(o) && iscolumn(o), observations)), 'each observation must be a numeric col vector');
@@ -16,7 +16,7 @@ function s = huge_states_from(observations)
         x  = [s(1:8); o(3); o(4); o(5)];
         u  = o(1:2) - s(1:2);
         ts = o(6:end);
-        
+
         %states{i+1} = [adp_transition_post_decision(x,u); ts];
         states{i+1} = [huge_trans_post(x,u); ts];
     end
